9a7ecdfdfc9e6efba2ccb5bb058067405c83f4e4
Christopher Bugtong <cbugtong@gmail.com>
2016-11-14 14:03:21 -0500

    Loose Implementation of Camera and Main Activity
    yayay

--- UCurate/app/src/main/java/com/example/jbrow/ucurate/MainActivity.java
+++ UCurate/app/src/main/java/com/example/jbrow/ucurate/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jbrow.ucurate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button cameraButton = (Button) findViewById(R.id.camera_button);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent openCamera = new Intent(MainActivity.this,CameraActivity.class);
+                startActivity(openCamera);
+            }
+        });
     }
 }
