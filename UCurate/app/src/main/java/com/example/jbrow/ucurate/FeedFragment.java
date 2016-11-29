50de7e9d5ed25e45418ce79498d0e43c781bc60b
Charmi Patel <charmip@umd.edu>
2016-11-29 10:39:16 -0500

    Created Explore Fragment
    created explore fragment

--- UCurate/app/src/main/java/com/example/jbrow/ucurate/FeedFragment.java
+++ UCurate/app/src/main/java/com/example/jbrow/ucurate/FeedFragment.java
@@ -1,21 +1,12 @@
 package com.example.jbrow.ucurate;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.example.jbrow.ucurate.dummy.DummyContent;
-import com.example.jbrow.ucurate.dummy.DummyContent.DummyItem;
-
-import java.util.List;
 
 
 public class FeedFragment extends ListFragment {
