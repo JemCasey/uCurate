d49e15a6958067abb6dc5304822bd514b7a60d42
JemCasey <jbrownst@terpmail.umd.edu>
2016-12-04 09:25:43 -0500

    Firebasing Tour Activities, uncommented DragListView in layout file

--- UCurate/app/src/main/java/com/example/jbrow/ucurate/NewTourActivity.java
+++ UCurate/app/src/main/java/com/example/jbrow/ucurate/NewTourActivity.java
@@ -14,7 +14,7 @@ public class NewTourActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        userID = intent.getStringExtra("userID");
+        userID = intent.getStringExtra(User.USER_ID);
 
         setContentView(R.layout.activity_new_collection);
 
@@ -28,9 +28,9 @@ public class NewTourActivity extends Activity {
                 Tour newTour = new Tour(tourName.getText().toString(), tourDescription.getText().toString(), userID);
                 FireBase.addTour("1", newTour);
                 Intent intent = new Intent(NewTourActivity.this, AddToTourActivity.class);
-                intent.putExtra("tour_name", tourName.getText());
-                intent.putExtra("tour_description", tourDescription.getText());
-
+                intent.putExtra(User.USER_ID, userID);
+                intent.putExtra(Tour.TOUR_NAME, tourName.getText());
+                intent.putExtra(Tour.TOUR_DESCRIPTION, tourDescription.getText());
                 startActivity(intent);
             }
         });
