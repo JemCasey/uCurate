1ba2a8fb6bea5db3cf7f6c2cffd33a252a48fa3b
Christopher Bugtong <cbugtong@gmail.com>
2016-12-03 22:54:43 -0500

    minor text fixes

--- UCurate/app/src/main/java/com/example/jbrow/ucurate/AddToTourActivity.java
+++ UCurate/app/src/main/java/com/example/jbrow/ucurate/AddToTourActivity.java
@@ -29,8 +29,8 @@ public class AddToTourActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_tour);
         ArrayList<Artwork> artworks = new ArrayList();
-        artworks.add(new Artwork("Betty", "this is by Gerhard Richter", new LatLng(38.984929, -76.947760)/*,BitmapFactory.decodeResource(getResources(),R.drawable.betty)*/));
-        artworks.add(new Artwork("Higher Beings Commanded: Paint the Upper-Right Corner Black!", "this is by Sigmar Polke", new LatLng(38.99, -76.947760)/*,BitmapFactory.decodeResource(getResources(),R.drawable.betty)*/));
+//        artworks.add(new Artwork("Betty", "this is by Gerhard Richter", new LatLng(38.984929, -76.947760)/*,BitmapFactory.decodeResource(getResources(),R.drawable.betty)*/));
+//        artworks.add(new Artwork("Higher Beings Commanded: Paint the Upper-Right Corner Black!", "this is by Sigmar Polke", new LatLng(38.99, -76.947760)/*,BitmapFactory.decodeResource(getResources(),R.drawable.betty)*/));
 
         dataAdapter = new ArtAdapter(this, R.layout.tour_item, artworks);
         ListView listView = (ListView) findViewById(R.id.listView1);
