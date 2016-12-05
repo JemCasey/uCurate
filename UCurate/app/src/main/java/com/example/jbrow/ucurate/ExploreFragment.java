0e8678761f6fd4136b2e879145c6a0431c841073
Matthew Hahne <MatthewHahne@gmail.com>
2016-12-04 19:35:42 -0500

    loaded data

--- UCurate/app/src/main/java/com/example/jbrow/ucurate/ExploreFragment.java
+++ UCurate/app/src/main/java/com/example/jbrow/ucurate/ExploreFragment.java
@@ -3,13 +3,24 @@ package com.example.jbrow.ucurate;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.ListFragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 
 public class ExploreFragment extends ListFragment {
+
+    ArrayList<Tour> exploreTours = new ArrayList<Tour>();
 
     ExploreItemAdapter mExploreItemAdapter;
 
@@ -20,6 +31,26 @@ public class ExploreFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("tours");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //mUser = dataSnapshot.getValue(User.class);
+                for(DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+                    for(DataSnapshot tourSnapshot : userSnapshot.getChildren()) {
+                            exploreTours.add(tourSnapshot.getValue(Tour.class));
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         String userId = getArguments().getString(User.USER_ID);
 
